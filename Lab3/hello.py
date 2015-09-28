2f9099e50ec4479c48d00bd4ea9b1abf5a311800
yomi96 <C13406652@mydit.ie>
2015-09-28 16:06:34 +0000

    inserted string will be uppercase

--- Lab3/hello.py
+++ Lab3/hello.py
@@ -1,8 +1,10 @@
 #this code shows if a word entered by user is a palindrome
-my_str = input("Enter a string: ")
+my_str = input("Enter a string: ").upper()
+
+
 my_str = my_str.casefold()
 rev_str = reversed(my_str)
-if list(my_str) == list(rev_str):
-  print("Its a palindrom")
-  else:
-    print("Its not a palindrome")
+if list(my_str) == list(rev_str): #if word is equal to word backwards
+  print("Its a palindrom") #print this if its palindrome
+else:
+  print("Its not a palindrome")
